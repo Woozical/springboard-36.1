0f5d0a6cd4957a0e0dd68f2cbd97e082fd24e400
Mark <mark@example.dev>
2021-10-02 21:00:12 -0700

    auth routes

--- models/user.js
+++ models/user.js
@@ -9,11 +9,22 @@ const ExpressError = require('../expressError');
 
 class User {
 
+  /* validates presence of object properties
+  *  returns 400 ExpressError if missing
+  */
+
+  static validateArgs(obj){
+    for (let arg in obj){
+      if (obj[arg] === undefined) throw new ExpressError(`Missing field: ${arg}`, 400);
+    }
+  }
+
   /** register new user -- returns
    *    {username, password, first_name, last_name, phone}
    */
 
   static async register({username, password, first_name, last_name, phone}){
+    User.validateArgs({username, password, first_name, last_name, phone});
     const hashedPass = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
     const result = await db.query(`
       INSERT INTO users (username, password, first_name, last_name, phone, join_at, last_login_at)
@@ -22,13 +33,14 @@ class User {
       [username, hashedPass, first_name, last_name, phone, new Date(), new Date()]);
     
     return result.rows[0];
-
   }
 
   /** Authenticate: is this username/password valid? Returns boolean. */
 
   static async authenticate(username, password) {
+    User.validateArgs({username, password});
     const result = await db.query('SELECT password FROM users WHERE username = $1', [username]);
+    console.log(result);
     if (result.rowCount < 1) throw new ExpressError('User not found', 404);
 
     const user = result.rows[0];
@@ -38,6 +50,7 @@ class User {
   /** Update last_login_at for user */
 
   static async updateLoginTimestamp(username) {
+    User.validateArgs({username});
     const result = await db.query(
       `UPDATE users SET last_login_at = $1 WHERE username = $2
       RETURNING *`, [new Date(), username]);
@@ -65,6 +78,7 @@ class User {
    *          last_login_at } */
 
   static async get(username) {
+    User.validateArgs({username});
     const result = await db.query(
       `SELECT username, first_name, last_name, phone, join_at, last_login_at FROM users
       WHERE username = $1`, [username]
@@ -82,6 +96,7 @@ class User {
    */
 
   static async messagesFrom(username) {
+    User.validateArgs({username});
     const result = await db.query(
       `SELECT messages.id, messages.body, messages.sent_at, messages.read_at,
       to_user.username, to_user.first_name, to_user.last_name, to_user.phone
@@ -108,6 +123,7 @@ class User {
    */
 
   static async messagesTo(username) {
+    User.validateArgs({username});
     const result = await db.query(
       `SELECT messages.id, messages.body, messages.sent_at, messages.read_at,
       from_user.username, from_user.first_name, from_user.last_name, from_user.phone
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,28 @@
+const express = require("express");
+const router = new express.Router();
+const User = require('../models/user');
+const ExpressError = require('../expressError');
+const jwt = require('jsonwebtoken');
+const { SECRET_KEY } = require('../config');
+
 /** POST /login - login: {username, password} => {token}
  *
  * Make sure to update their last-login!
  *
  **/
+router.post('/login', async (req, res, next) => {
+    try{
+        const { username, password } = req.body;
+        if (await User.authenticate(username, password)){
+            User.updateLoginTimestamp(username);
+            const token = jwt.sign({user : username}, SECRET_KEY);
+            return res.json({token});
+        }
+        throw new ExpressError('Unauthorized', 401);
+    } catch (err) {
+        return next(err);
+    }
+});
 
 
 /** POST /register - register user: registers, logs in, and returns token.
@@ -11,3 +31,17 @@
  *
  *  Make sure to update their last-login!
  */
+
+router.post('/register', async (req, res, next) => {
+    try{
+        const user = await User.register(req.body);
+        User.updateLoginTimestamp(user.username);
+        const token = jwt.sign({user : user.username}, SECRET_KEY);
+        return res.json({token});
+    } catch (err) {
+        if (err.code === "23505") return next(new ExpressError('Username already exists', 400))
+        return next(err);
+    }
+});
+
+module.exports = router;
